49fa1509edaedf272bb45c1166258b2013de4bb1
Sydnee Stannis baratheon <sstannis@e4r9p9.21-school.ru>
2019-02-11 18:19:46 +0300

    p and u

--- handler_u.c
+++ handler_u.c
@@ -5,11 +5,10 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sstannis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/10 16:26:26 by sstannis          #+#    #+#             */
-/*   Updated: 2019/02/10 16:26:28 by sstannis         ###   ########.fr       */
+/*   Created: 2019/02/05 16:38:04 by sstannis          #+#    #+#             */
+/*   Updated: 2019/02/05 16:38:06 by sstannis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "header.h"
 #include "libft/includes/libft.h"
@@ -63,7 +62,6 @@ static char	*hex_sharp(char *str, t_mod *zeus)
 	char	*str_sharp;
 	char	*tmp;
 
-    printf("%s\n", str);
 	str_sharp = "0x";
 	tmp = str;
 	str = ft_strjoin(str_sharp, str);
@@ -72,18 +70,17 @@ static char	*hex_sharp(char *str, t_mod *zeus)
 }
 
 //void	ft_hex(va_list ap, char **str, t_mod *zeus)
-void	ft_pnt(char **str, t_mod *zeus, void *point)
+void	ft_udc(char **str, t_mod *zeus, unsigned int l)
 {
-	unsigned long int		hex;
+	unsigned int		hex;
 	char	*string;
-
-	hex = (unsigned long int)point;
+	
+	hex = l;
 	//hex = va_arg(ap, int);
-	printf("(%lu)\n", hex);
-	string = ft_itoal(hex, 16, zeus);
+	string = ft_itoal(hex, 10, zeus);
 	if (zeus->precision)
 		string = hex_precision(string, zeus, 1);
-	if (1)
+	if (zeus->sharp)
 		string = hex_sharp(string, zeus);
 	if (zeus->zero && !(zeus->precision) && zeus->min_width)
 		string = hex_precision(string, zeus, 0);
