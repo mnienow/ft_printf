5fa27c38d9877d2f2f19242803e18bdbc8b58ff4
Sydnee Stannis baratheon <sstannis@e4r15p3.21-school.ru>
2019-03-04 18:59:14 +0300

    1

---
+++ handler_f.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handler_o.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sstannis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/05 16:38:04 by sstannis          #+#    #+#             */
+/*   Updated: 2019/02/05 16:38:06 by sstannis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int		stepen(int	i)
+{
+	int		ten;
+
+	ten = 10;
+	
+}
+
+void	ft_dbl(char **str, t_mod *zeus, va_list ap)
+{
+    double      number;
+    int         precision;
+    char		*string;
+
+    precision = 6;
+    if (zeus->precision)
+        precision = zeus->precision;
+    number = va_arg(ap, double);
+    zeus->alpha = 0;
+    string = ft_itoal((int)number, 10, zeus);
+    *str = ft_strjoin(*str, string);
+    number = (number - (int)number) * 10000 + 0.1;
+    *str = ft_strjoin(*str, ".");
+    string = ft_itoal((int)number, 10, zeus);
+    *str = ft_strjoin(*str, string);
+    free(string);
+}
